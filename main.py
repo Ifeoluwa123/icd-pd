b3aa4704dc86433c01db3e5a2fd21b4433fe0e79
Adeleye Ifeoluwa Seun <35472628+Ifeoluwa123@users.noreply.github.com>
2026-01-18 13:30:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import os
+from huggingface_hub import InferenceClient
+from pydantic import BaseModel
+
+app = FastAPI() 
+os.environ["HF_TOKEN"] = "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"
+client = InferenceClient()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastA"}
+
+
+@app.get("/items/{item_id}")
+
+def read_item(item_id: int, q:str | None = None):
+    return {"item_id": item_id, "q":q}
+
+
+
+class Input(BaseModel):
+    input: str
+
+@app.post('/predict')
+def predic_genai(input:Input):
+    messages = [
+        {"role": "system", "content": "You are a AI assistant."},
+         {"role": "user", "content": input.input}
+    ]
+    response = client.chat_completion(
+          model="zai-org/GLM-4.7",
+    messages=messages
+    )
+    return {"Response": response["choices"][0]["message"]["content"]}
